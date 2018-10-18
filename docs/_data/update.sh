6c902655e60c2f95364f1a4679cd7b2fc61b4bd0
Andrew Chen <yongjhih@gmail.com>
2018-10-19 00:28:35 +0800

    Add update.sh

---
+++ docs/_data/update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#* Mender Deployment Service: https://github.com/mendersoftware/deployments
+#* Mender Device Inventory Service:
+#  - https://github.com/mendersoftware/inventory
+#  - https://github.com/mendersoftware/deviceadm
+#* Mender User Administration Service: https://github.com/mendersoftware/useradm
+
+#wget https://github.com/mendersoftware/inventory/raw/master/docs/management_api.yml -O inventory.yml
+#wget https://github.com/mendersoftware/useradm/raw/master/docs/management_api.yml -O useradm.yml
+#wget https://github.com/mendersoftware/deviceadm/raw/master/docs/management_api.yml -O deviceadm.yml
+#wget https://github.com/mendersoftware/deployments/raw/master/docs/management_api.yml -O deployments.yml
+
+# ymlToJson
+#curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "@${i}" "https://generator.swagger.io/api/gen/clients/swagger-yaml"
+
+for i in *.yml; do
+    name="${i%.yml}"
+    swagger-codegen generate -i "${i}" -l swagger && mv swagger.json "${name}.json"
+    [ -f "README.md" ] && rm README.md
+    swagger-codegen-axios-flow < "${name}.json" > "${name}.js"
+done
+
+#for i in *.json; do
+#    local name="${i%.json}"
+#    swagger-codegen-axios-flow < "${name}.json" > "${name}.js"
+#done
